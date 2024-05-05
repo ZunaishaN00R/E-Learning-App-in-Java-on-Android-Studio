c9445317c9a5625b5878dfab12e7609e635c11c5
Usmana <usmanazulfiqar2001@gmail.com>
2024-05-05 06:05:10 +0500

    quiz updation

---
+++ app/src/main/java/com/zunaisha/e_learning_app/ui/quiz/ScoreActivity.java
@@ -0,0 +1,16 @@
+package com.zunaisha.e_learning_app.ui.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zunaisha.e_learning_app.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
